d9b250cc99380a7839924874c05f105ca26dc1fe
Alberto <ansmorales@gmail.com>
2022-07-19 17:32:42 +0200

    Añadida pantalla creditos

--- app/src/main/java/com/anmoraque/eldesaviodominguerojerez/PantallaCreditosActivity.java
+++ app/src/main/java/com/anmoraque/eldesaviodominguerojerez/PantallaCreditosActivity.java
@@ -2,10 +2,17 @@ package com.anmoraque.eldesaviodominguerojerez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PantallaCreditosActivity extends AppCompatActivity {
+
+    private ImageView logo_linkedin;
+    private ImageView logo_github;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,30 @@ public class PantallaCreditosActivity extends AppCompatActivity {
         //Boton de ir atras
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        this.logo_linkedin = findViewById(R.id.logo_linkedin);
+        this.logo_github = findViewById(R.id.logo_github);
+
+        this.logo_linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url_linkedin = "https://www.linkedin.com/in/alberto-morales-ab373a235/";
+                Intent intent_abrir_web = new Intent();
+                intent_abrir_web.setAction(Intent.ACTION_VIEW);
+                intent_abrir_web.setData(Uri.parse(url_linkedin));
+                view.getContext().startActivity(intent_abrir_web);
+            }
+        });
+        this.logo_github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url_linkedin = "https://github.com/anmoraque";
+                Intent intent_abrir_web = new Intent();
+                intent_abrir_web.setAction(Intent.ACTION_VIEW);
+                intent_abrir_web.setData(Uri.parse(url_linkedin));
+                view.getContext().startActivity(intent_abrir_web);
+            }
+        });
     }
 
     // Metodo para la accion del boton de ir atras
