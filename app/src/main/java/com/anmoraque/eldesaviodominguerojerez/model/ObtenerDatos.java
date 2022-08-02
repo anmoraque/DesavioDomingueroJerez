02dbb8a9e0576d33dddd1b2f6e6ef16c3e89fabb
Alberto <ansmorales@gmail.com>
2022-08-02 16:23:41 +0200

    Actualizado url 3

--- app/src/main/java/com/anmoraque/eldesaviodominguerojerez/model/ObtenerDatos.java
+++ app/src/main/java/com/anmoraque/eldesaviodominguerojerez/model/ObtenerDatos.java
@@ -18,7 +18,6 @@ import com.anmoraque.eldesaviodominguerojerez.MapsActivity;
 import com.google.gson.reflect.TypeToken;
 import com.anmoraque.eldesaviodominguerojerez.PantallaNegociosActivity;
 import com.google.gson.Gson;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
